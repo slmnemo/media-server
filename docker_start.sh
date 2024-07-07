0fdae7a2b7860ecf5470b4bf66c2a42100e7fe42
omenmis <nicholas.lucioforlife@yahoo.com>
2024-07-07 12:13:55 -0700

    Initial commit for media server

---
+++ docker_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Wrapper script to import user settings, call docker based on docker-compose.yaml and then background it.
+
+# Launch docker
+docker-compose up -d
+
---
+++ scheduled_restart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./man_server restart
+docker restart jellyfin
